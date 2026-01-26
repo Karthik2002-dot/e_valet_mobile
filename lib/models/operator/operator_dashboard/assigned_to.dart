d5151d6d94952b467426b0f89c86c2386b739a17
ManjuH98 <HManjunath@yathisolutions.com>
2026-01-26 21:26:39 +0530

    EVA42: Driver Status Issue Resolved

--- lib/models/operator/operator_dashboard/assigned_to.dart
+++ lib/models/operator/operator_dashboard/assigned_to.dart
@@ -1,22 +1,26 @@
 class AssignedTo {
   final String userId;
   final String name;
+  final String phone;
 
   AssignedTo({
     required this.userId,
     required this.name,
+    required this.phone,
   });
 
   factory AssignedTo.fromJson(Map<String, dynamic> json) {
     return AssignedTo(
       userId: json['userId'] ?? '',
       name: json['name'] ?? '',
+      phone: json['phone'] ?? '',
     );
   }
   Map<String, dynamic> toJson() {
     return {
       'userId': userId,
       'name': name,
+      'phone': phone,
     };
   }
 }
